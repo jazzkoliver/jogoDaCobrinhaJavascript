abdfba38fd2a4008997fddf01be444cfa984e47a
jazzkoliver <jazzkoliver@gmail.com>
2021-08-10 13:58:53 -0300

    estilizando body

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let canvas = document.getElementById("snake");
+let canvas = document.getElementById("snake"); //criando elemento que irá rodar o jogo
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
