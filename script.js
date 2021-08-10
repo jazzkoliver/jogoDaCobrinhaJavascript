cae1c265e6fee150804841768309e6d4fd5cfd98
jazzkoliver <jazzkoliver@gmail.com>
2021-08-10 14:59:31 -0300

    alterando cores

--- script.js
+++ script.js
@@ -14,19 +14,19 @@ let food = {
 }
 
 function criarBG() {
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "lightgrey";
     context.fillRect(0, 0, 16 * box, 16 * box); //desenha o retângulo usando x e y e a largura e altura setadas
 }
 
 function criarCobrinha() {
     for(i = 0; i < snake.length; i++) {
-        context.fillStyle = "green";
+        context.fillStyle = "black";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
 function drawFood() {
-    context.fillStyle = "red";
+    context.fillStyle = "rebeccapurple";
     context.fillRect(food.x, food.y, box, box);
 }
 
